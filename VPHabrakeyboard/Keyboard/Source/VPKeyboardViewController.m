b743533a8a67d2307a4bb2cc543c556d34d804e2
Vladimir Popko <vladimirpopko@yandex.ru>
2014-09-08 18:26:32 +0300

    Implemented logic for entering habra-tags to field

--- VPHabrakeyboard/Keyboard/Source/VPKeyboardViewController.m
+++ VPHabrakeyboard/Keyboard/Source/VPKeyboardViewController.m
@@ -11,6 +11,7 @@
 @interface VPKeyboardViewController ()
 
 @property (nonatomic, strong) IBOutlet UIView *keyboardView;
+@property (nonatomic, strong) NSDictionary *tagsDictionary;
 
 @end
 
@@ -22,19 +23,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self.class) owner:self options:nil];
-    [self.view addSubview:self.keyboardView];
-}
-
-#pragma mark -
-#pragma mark UITextInputDelegate
-
-- (void)textWillChange:(id<UITextInput>)textInput {
-    [super textWillChange:textInput];
-}
-
-- (void)textDidChange:(id<UITextInput>)textInput {
-    [super textDidChange:textInput];
+    [self loadKeyboard];
+    [self loadTags];
 }
 
 #pragma mark -
@@ -54,6 +44,13 @@
     [self dismissKeyboard];
 }
 
+- (IBAction)onHabraButtonPressed:(id)sender {
+    NSString *tagKey = [sender titleForState:UIControlStateNormal];
+    NSString *tagValue = self.tagsDictionary[tagKey];
+    [self.textDocumentProxy insertText:tagValue];
+    [self moveTextPositionToInputPointForTag:tagValue];
+}
+
 - (IBAction)onLeftSwipeRecognized:(id)sender {
     if (self.textDocumentProxy.documentContextBeforeInput.length > 0) {
         [self.textDocumentProxy adjustTextPositionByCharacterOffset:-1];
@@ -63,6 +60,37 @@
 - (IBAction)onRightSwipeRecognized:(id)sender {
     if (self.textDocumentProxy.documentContextAfterInput.length > 0) {
         [self.textDocumentProxy adjustTextPositionByCharacterOffset:1];
+    }
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (void)loadKeyboard {
+    [[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self.class) owner:self options:nil];
+    [self.view addSubview:self.keyboardView];
+}
+
+- (void)loadTags {
+    NSString *tagsFilePath = [[NSBundle mainBundle] pathForResource:@"tags" ofType:@"json"];
+    NSData *tagsData = [NSData dataWithContentsOfFile:tagsFilePath];
+    NSError *error = nil;
+    self.tagsDictionary = [NSJSONSerialization JSONObjectWithData:tagsData options:NSJSONReadingAllowFragments error:&error];
+    NSAssert(error == nil, @"Invalid data format in file: %@", tagsFilePath);
+}
+
+- (void)moveTextPositionToInputPointForTag:(NSString *)tag {
+    NSArray *inputPointLabels = @[@"://", @"=\"", @">"];
+    for (NSString *label in inputPointLabels) {
+        NSRange labelRange = [tag rangeOfString:label];
+        if (labelRange.location != NSNotFound) {
+            NSInteger offset = labelRange.location + labelRange.length - tag.length;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                // Hack: Call this code after delay. In other case these changes won't be applied
+                [self.textDocumentProxy adjustTextPositionByCharacterOffset:offset];
+            });
+            break;
+        }
     }
 }
 
